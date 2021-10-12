9c77cd94e7eb6282727255610a5b7bf03fb1a3c9
trevormw20 <45832143+trevormw20@users.noreply.github.com>
2021-10-11 23:47:38 -0600

    Added Search function. -Still need to add enum to fill the combobox and  make everthing look nicer.

--- MegaDesk/SearchQuotes.Designer.cs
+++ MegaDesk/SearchQuotes.Designer.cs
@@ -31,27 +31,55 @@ namespace MegaDesk_Schutz
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchQuotes));
             this.searchQuotesBackButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // searchQuotesBackButton
             // 
-            this.searchQuotesBackButton.Location = new System.Drawing.Point(713, 415);
+            this.searchQuotesBackButton.Location = new System.Drawing.Point(1019, 692);
+            this.searchQuotesBackButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.searchQuotesBackButton.Name = "searchQuotesBackButton";
-            this.searchQuotesBackButton.Size = new System.Drawing.Size(75, 23);
+            this.searchQuotesBackButton.Size = new System.Drawing.Size(107, 38);
             this.searchQuotesBackButton.TabIndex = 2;
             this.searchQuotesBackButton.Text = "Back";
             this.searchQuotesBackButton.UseVisualStyleBackColor = true;
             this.searchQuotesBackButton.Click += new System.EventHandler(this.searchQuotesBackButton_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Oak",
+            "Veneer",
+            "Rosewood"});
+            this.comboBox1.Location = new System.Drawing.Point(68, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 33);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(349, 115);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(618, 548);
+            this.panel1.TabIndex = 4;
+            // 
             // SearchQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1134, 722);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.searchQuotesBackButton);
-            this.MaximumSize = new System.Drawing.Size(816, 489);
-            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(1156, 778);
+            this.MinimumSize = new System.Drawing.Size(1156, 778);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
             this.ResumeLayout(false);
@@ -61,5 +89,7 @@ namespace MegaDesk_Schutz
         #endregion
 
         private System.Windows.Forms.Button searchQuotesBackButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- MegaDesk/SearchQuotes.cs
+++ MegaDesk/SearchQuotes.cs
@@ -21,5 +21,69 @@ namespace MegaDesk_Schutz
             viewMainMenu.Show();
             this.Close();
         }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+
+            MainMenu.getAllDeskQuotes();
+            int count = 0;
+            foreach (DeskQuote quote in MainMenu.deskQuotes)
+            {
+               
+
+                if (quote.customerDesk.material.ToString() == comboBox1.SelectedItem.ToString())
+                {
+
+                    // Create panel
+                    Panel quotePanel = new Panel();
+                    quotePanel.Location = new System.Drawing.Point(0, (0 + (335 * count)));
+                    quotePanel.Size = new System.Drawing.Size(519, 321);
+                    quotePanel.BackColor = Color.WhiteSmoke;
+
+                    // User Name Label
+                    Label userNameLabel = new Label();
+                    userNameLabel.Text = "Name: " + quote.customerName;
+                    userNameLabel.Location = new Point(133, 12);
+                    userNameLabel.Size = new Size(280, 23);
+                    userNameLabel.BackColor = Color.Gainsboro;
+                    userNameLabel.TextAlign = ContentAlignment.MiddleLeft;
+
+                    // Date Label
+                    Label dateLabel = new Label();
+                    dateLabel.Text = "Date: " + quote.quoteDate;
+                    dateLabel.Location = new Point(133, 80);
+                    dateLabel.Size = new Size(280, 23);
+                    dateLabel.BackColor = Color.Gainsboro;
+                    dateLabel.TextAlign = ContentAlignment.MiddleLeft;
+
+                    // Specs Label
+                    Label specsLabel = new Label();
+                    specsLabel.Text = "Specifications \n Width: " + quote.customerDesk.width + "\n Depth: " + quote.customerDesk.depth +
+                                      "\n Drawers: " + quote.customerDesk.drawers + "\n Material: " + quote.customerDesk.material;
+                    specsLabel.Location = new Point(133, 150);
+                    specsLabel.Size = new Size(280, 150);
+                    specsLabel.BackColor = Color.Gainsboro;
+                    specsLabel.TextAlign = ContentAlignment.MiddleLeft;
+
+                    // Overall Price Label
+                    Label overallPriceLabel = new Label();
+                    overallPriceLabel.Text = "Total Quote Price: $" + quote.totalCost;
+                    overallPriceLabel.Location = new Point(133, 300);
+                    overallPriceLabel.Size = new Size(280, 23);
+                    overallPriceLabel.BackColor = Color.Gainsboro;
+                    overallPriceLabel.TextAlign = ContentAlignment.MiddleLeft;
+
+                    quotePanel.Controls.Add(userNameLabel);
+                    quotePanel.Controls.Add(dateLabel);
+                    quotePanel.Controls.Add(specsLabel);
+                    quotePanel.Controls.Add(overallPriceLabel);
+
+                    panel1.Controls.Add(quotePanel);
+                    count++;
+                }
+                
+            }
+        }
     }
 }
